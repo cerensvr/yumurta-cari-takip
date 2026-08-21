716634c2bc4d2cda372d24f7d0c861a2d9c35f99
Ceren Sivri <cerensivri@Ceren-MacBook-Pro.local>
2026-08-21 16:01:41 +0300

    deploy v1.0.11 build 16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "676083652" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3686720914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
